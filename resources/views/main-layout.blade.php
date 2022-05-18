2ee8cf37b84a125d6db467c00841575b420a922f
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-18 16:30:16 +0200

    create sezioni del layout generale

---
+++ resources/views/main-layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC-COMICS</title>
+</head>
+<body>
+    <header></header>
+
+    <main>
+        <div id="card-section"></div>
+        <div id="merchandising-section"></div>
+        <div id="contact-section"></div>
+    </main>
+
+    <footer></footer>
+</body>
+</html>
