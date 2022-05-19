d1b1a3f9e01977f05d877685201c81f16597d2dc
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-19 20:34:00 +0200

    modifica

--- resources/views/cardDetail.blade.php
+++ resources/views/cardDetail.blade.php
@@ -2,9 +2,12 @@
 @extends('layouts.layoutDetail')
 
 @section('contentDetail')
-<div class="card-item">
-    <img src="{{ $fumetti['thumb'] }}" alt="">
-    <span>{{ $fumetti['series'] }}</span>
-</div>
+<div class="card-container">
+         <div class="card-item">
+                 <img src="{{ $fumetti['thumb'] }}" alt="">
+                 <span>{{ $fumetti['series'] }}</span>
+             </a>
+         </div>
+ </div>
 @endsection
 
--- resources/views/layouts/layoutDetail.blade.php
+++ resources/views/layouts/layoutDetail.blade.php
@@ -1,3 +1,17 @@
-<div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--collego il mio css-->
+    <link rel="stylesheet" href="css/app.css">
+    <title>Dettagli del Film</title>
+</head>
+<body>
     @yield('contentDetail')
-</div>
+</body>
+</html>
+
+
+   
