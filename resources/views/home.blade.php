ebfdbb575419bff697f694657de2edec85155abb
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-19 18:44:32 +0200

    modifica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.layout')
 
 @section('content')
-<h2>Lista dei fumetti</h2>
-<ul>
+<!--h2>Lista dei fumetti</h2-->
     <!--@dump($fumetti)-->
 
     <!--@foreach($fumetti as $fumetto)
     <li>{{ $fumetto['title'] }}</li>
     @endforeach-->
     <div class="card-container">
-        @foreach($fumetti as $fumetto)
+         @foreach($fumetti as $fumetto)
         <div class="card-item">
             <img src="{{ $fumetto['thumb'] }}" alt="">
             <span>{{ $fumetto['series'] }}</span>
-        </div>
-        @endforeach
+         </div>
+         @endforeach
     </div>
-
-</ul>
 @endsection
